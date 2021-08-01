bf02516df6a072e1a190c7fce7061783586985cd
imti283 <imtiyaz283@gmail.com>
2021-08-01 14:14:02 +0530

    pwsh to install programm using msiexec

---
+++ PowerShell/install_msi.ps1
@@ -0,0 +1,12 @@
+$file = "<MSI Path here>"
+$DataStamp = get-date -Format yyyyMMddTHHmmss
+$logFile = '{0}-{1}.log' -f $file.fullname,$DataStamp
+$MSIArguments = @(
+    "/i"
+    ('"{0}"' -f $file.fullname)
+    "/qn"
+    "/norestart"
+    "/L*v"
+    $logFile
+)
+Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow 
