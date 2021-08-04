79ef5c5cd219d6d0ba6377aa0950a79dfb91ea79
imti283 <imtiyaz283@gmail.com>
2021-08-04 18:34:14 +0530

    delete older logs

---
+++ PowerShell/delete_olderlogs.ps1
@@ -0,0 +1,6 @@
+$Path = "D:\Logs\W3SVC1"
+$Daysback = "-6"
+ 
+$CurrentDate = Get-Date
+$DatetoDelete = $CurrentDate.AddDays($Daysback)
+Get-ChildItem $Path | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item
